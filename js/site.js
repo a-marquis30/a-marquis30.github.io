65492c24b8379ec95e0071ccd7c9050f845d77a7
a-marquis30 <austinmarquis30@gmail.com>
2024-01-30 14:49:45 -0500

    Added favicon and scrollable content.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,27 @@
 document.addEventListener('mousemove', e => {
-    document.body.style.setProperty('--x', `${e.clientX}px`);
-    document.body.style.setProperty('--y', `${e.clientY}px`);
+  document.body.style.setProperty('--x', `${e.pageX}px`);
+  document.body.style.setProperty('--y', `${e.pageY}px`);
+});
+
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  let scrollY = window.pageYOffset;
+  
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+    
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+    }
   });
+}
