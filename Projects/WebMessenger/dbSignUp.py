923448373004fca52ec3973a175610e78fd5cc6f
a-marquis30 <austinmarquis30@gmail.com>
2024-02-06 15:10:13 -0500

    Add port to db connection.

--- Projects/WebMessenger/dbSignUp.py
+++ Projects/WebMessenger/dbSignUp.py
@@ -19,10 +19,13 @@ def handle_post():
     db_user = os.environ.get('DB_USER')
     db_password = os.environ.get('DB_PASSWORD')
     db_host = os.environ.get('DB_HOST')
+    db_port = os.environ.get('DB_PORT')
     db_name = os.environ.get('DB_NAME')
 
     # create a connection to the database
     cnx = mysql.connector.connect(user=db_user, password=db_password, host=db_host, database=db_name)
+    cnx = mysql.connector.connect(user=db_user, password=db_password, host=db_host, port=db_port, database=db_name)
+
 
     # create a cursor object
     cursor = cnx.cursor()
