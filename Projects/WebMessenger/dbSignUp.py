04723072957fceda2f84e6dac9e799298d3e552d
a-marquis30 <austinmarquis30@gmail.com>
2024-02-08 14:57:14 -0500

    Convert port to int.

--- Projects/WebMessenger/dbSignUp.py
+++ Projects/WebMessenger/dbSignUp.py
@@ -19,7 +19,7 @@ def handle_post():
     db_user = os.environ.get('DB_USER')
     db_password = os.environ.get('DB_PASSWORD')
     db_host = os.environ.get('DB_HOST')
-    db_port = os.environ.get('DB_PORT')
+    db_port = int(os.environ.get('DB_PORT'))
     db_name = os.environ.get('DB_NAME')
 
     # create a connection to the database
