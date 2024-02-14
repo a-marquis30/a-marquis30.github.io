d426e18c8ef5623911f0a88365635f080c7e00fc
a-marquis30 <austinmarquis30@gmail.com>
2024-02-14 16:41:28 -0500

    Change database connection.

--- Projects/WebMessenger/dbSignUp.py
+++ Projects/WebMessenger/dbSignUp.py
@@ -19,20 +19,14 @@ def handle_post():
     db_user = os.environ.get('DB_USER')
     db_password = os.environ.get('DB_PASSWORD')
     db_host = os.environ.get('DB_HOST')
-    db_port = int(os.environ.get('DB_PORT'))
     db_name = os.environ.get('DB_NAME')
 
     # create a connection to the database
     cnx = MySQLdb.connect(
         host = db_host,
-        port = db_port,
         user = db_user,
         password = db_password,
-        database = db_name,
-        ssl_mode = "VERIFY_IDENTITY",
-        ssl = {
-            "ca": "/home/austinmarquis30/mysite/isrgrootx1.pem"
-        }
+        database = db_name
     )
 
 
